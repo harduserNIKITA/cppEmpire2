06dd885d2c64433238f67472ee68edd4a87ddc4c
harduserNIKITA <cumpotttt@yandex.ru>
2023-10-16 10:41:32 +0300

    we are strong!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
-
+#include <string>
 int main()
 {
     std::cout << "We come back!\n";
+    std::cout << "What is our slogan of Empire\n";
+    std::string inputStr = "";
+    std::getline(std::cin, inputStr);
+    std::cout << inputStr + "Indeed.\n";
     return 0;
 }
